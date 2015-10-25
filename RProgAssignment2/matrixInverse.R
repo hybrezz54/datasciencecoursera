c9c311b3227ab4839ea8854f5a604e5d653d1f71
Hamzah Aslam <haz540@gmail.com>
2015-10-24 23:20:06 -0400

    Update 10/24/2015

--- RProgAssignment2/matrixInverse.R
+++ RProgAssignment2/matrixInverse.R
@@ -1,3 +1,8 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -11,6 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
        setInverse = setInverse,
        getInverse = getInverse)
 }
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   i <- x$getInverse()
